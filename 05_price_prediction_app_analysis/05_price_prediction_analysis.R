d66bc2100e2e3e767169c9938fc02c811c035327
Jason Wilcox <jason@Apples-iMac-Pro.local>
2019-11-10 23:02:37 -0700

    Modeule 5, prediction logic

--- 05_price_prediction_app_analysis/05_price_prediction_analysis.R
+++ 05_price_prediction_app_analysis/05_price_prediction_analysis.R
@@ -311,17 +311,90 @@ new_bike_tbl %>% format_table()
 
 # 8.0 OUTPUT PLOT PRODUCTS ----
 
-
+bikes_tbl
 
 
 # 8.1 bind_bike_predictions() function ----
 
+bind_bike_prediction <- function(bikes_tbl, new_bike_tbl){
+bikes_tbl %>%
+    separate_bike_description() %>%
+    mutate(estimate = "Actual") %>%
+    bind_rows(
+        new_bike_tbl %>% mutate(estimate = "Prediction")
+    ) %>%
+    select(estimate, model, category_1, category_2, frame_material, price)
+}
+
+bind_bike_prediction(bikes_tbl, new_bike_tbl)
+
 
 # 8.2 plot_bike_prediction() function ----
 
+plot_bike_prediction <- function(data, interactive = TRUE) {
+    
+    g <- data %>%
+        mutate(category_2 = fct_reorder(category_2, price)) %>%
+        mutate(label_text = str_glue("Price: {scales::dollar(price, accuracy = 1)}
+                                 Model: {model}
+                                 Bike Type: {category_1}")) %>%
+        
+        ggplot((aes(category_2, price, color= estimate))) +
+        
+        coord_flip() +
+        
+        geom_violin() +
+        
+        geom_jitter(aes(text = label_text), width = .1, alpha = .5) +
+        
+        facet_wrap(~ frame_material) +
+        
+        scale_y_log10(labels = scales::dollar_format(accruacy = 1)) +
+        
+        theme_tq()+
+        theme(strip.text.x = element_text(margin = margin(5,5,5,5))) +
+        labs(title = "", x = "", y = "")
+    
+    if (interactive) {
+        return(ggplotly(g, tooltip = "text"))
+    } else {
+        return(g)
+    }
+    
+    
+}
+
+# g <- bind_bike_prediction(bikes_tbl, new_bike_tbl) %>%
+#     
+#     mutate(category_2 = fct_reorder(category_2, price)) %>%
+#     mutate(label_text = str_glue("Price: {scales::dollar(price, accuracy = 1)}
+#                                  Model: {model}
+#                                  Bike Type: {category_1}")) %>%
+#     
+#     ggplot((aes(category_2, price, color= estimate))) +
+#     
+#     coord_flip() +
+#     
+#     geom_violin() +
+#     
+#     geom_jitter(aes(text = label_text), width = .1, alpha = .5) +
+#     
+#     facet_wrap(~ frame_material) +
+#     
+#     scale_y_log10(labels = scales::dollar_format(accruacy = 1)) +
+#     
+#     theme_tq()+
+#     theme(strip.text.x = element_text(margin = margin(5,5,5,5))) +
+#     labs(title = "", x = "", y = "")
+#     
+#     
+# 
+# ggplotly(g, tooltip = "text")
+
+bind_bike_prediction(bikes_tbl, new_bike_tbl) %>% plot_bike_prediction(interactive =  TRUE)
 
 # 8.3 Save functions ----
 
 dump(c("generate_new_bike", "format_table", "bind_bike_prediction", "plot_bike_prediction"), 
-     file = "00_scripts/02_make_predictions.R")
+     file = "00_scripts/03_make_predictions.R")
     
